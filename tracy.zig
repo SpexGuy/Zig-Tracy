88148745d54f996f3bdf9ee875678b64bcf4753e
avokadoen <akselhallajs@gmail.com>
2022-02-18 13:33:39 +0100

    mark unused variables in stub

--- tracy.zig
+++ tracy.zig
@@ -34,61 +34,213 @@ usingnamespace if (enabled) tracy_full else tracy_stub;
 
 const tracy_stub = struct {
     pub const ZoneCtx = struct {
-        pub inline fn Text(self: ZoneCtx, text: []const u8) void {}
-        pub inline fn Name(self: ZoneCtx, name: []const u8) void {}
-        pub inline fn Value(self: ZoneCtx, value: u64) void {}
-        pub inline fn End(self: ZoneCtx) void {}
+        pub inline fn Text(self: ZoneCtx, text: []const u8) void {
+            _ = self;
+            _ = text;
+        }
+        pub inline fn Name(self: ZoneCtx, name: []const u8) void {
+            _ = self;
+            _ = name;
+        }
+        pub inline fn Value(self: ZoneCtx, value: u64) void {
+            _ = self; 
+            _ = value;
+        }
+        pub inline fn End(self: ZoneCtx) void {
+            _ = self;
+        }
     };
 
     pub inline fn InitThread() void { }
-    pub inline fn SetThreadName(name: [*:0]const u8) void { }
+    pub inline fn SetThreadName(name: [*:0]const u8) void { 
+        _ = name;
+    }
     
-    pub inline fn Zone(comptime src: Src) ZoneCtx { return .{}; }
-    pub inline fn ZoneN(comptime src: Src, name: [*:0]const u8) ZoneCtx { return .{}; }
-    pub inline fn ZoneC(comptime src: Src, color: u32) ZoneCtx { return .{}; }
-    pub inline fn ZoneNC(comptime src: Src, name: [*:0]const u8, color: u32) ZoneCtx { return .{}; }
-    pub inline fn ZoneS(comptime src: Src, depth: i32) ZoneCtx { return .{}; }
-    pub inline fn ZoneNS(comptime src: Src, name: [*:0]const u8, depth: i32) ZoneCtx { return .{}; }
-    pub inline fn ZoneCS(comptime src: Src, color: u32, depth: i32) ZoneCtx { return .{}; }
-    pub inline fn ZoneNCS(comptime src: Src, name: [*:0]const u8, color: u32, depth: i32) ZoneCtx { return .{}; }
+    pub inline fn Zone(comptime src: Src) ZoneCtx { 
+        _ = src;
+        return .{}; 
+    }
+    pub inline fn ZoneN(comptime src: Src, name: [*:0]const u8) ZoneCtx { 
+        _ = src; 
+        _ = name;
+        return .{}; 
+    }
+    pub inline fn ZoneC(comptime src: Src, color: u32) ZoneCtx { 
+        _ = src; 
+        _ = color;
+        return .{}; 
+    }
+    pub inline fn ZoneNC(comptime src: Src, name: [*:0]const u8, color: u32) ZoneCtx { 
+        _ = src;
+        _ = name;
+        _ = color;
+        return .{}; 
+    }
+    pub inline fn ZoneS(comptime src: Src, depth: i32) ZoneCtx { 
+        _ = src;
+        _ = depth;
+        return .{}; 
+    }
+    pub inline fn ZoneNS(comptime src: Src, name: [*:0]const u8, depth: i32) ZoneCtx { 
+        _ = src;
+        _ = name;
+        _ = depth;
+        return .{}; 
+    }
+    pub inline fn ZoneCS(comptime src: Src, color: u32, depth: i32) ZoneCtx { 
+        _ = src;
+        _ = color;
+        _ = depth;
+        return .{}; 
+    }
+    pub inline fn ZoneNCS(comptime src: Src, name: [*:0]const u8, color: u32, depth: i32) ZoneCtx { 
+        _ = src;
+        _ = name;
+        _ = color;
+        _ = depth;
+        return .{}; 
+    }
 
-    pub inline fn Alloc(ptr: ?*const c_void, size: usize) void { }
-    pub inline fn Free(ptr: ?*const c_void) void { }
-    pub inline fn SecureAlloc(ptr: ?*const c_void, size: usize) void { }
-    pub inline fn SecureFree(ptr: ?*const c_void) void { }
-    pub inline fn AllocS(ptr: ?*const c_void, size: usize, depth: c_int) void { }
-    pub inline fn FreeS(ptr: ?*const c_void, depth: c_int) void { }
-    pub inline fn SecureAllocS(ptr: ?*const c_void, size: usize, depth: c_int) void { }
-    pub inline fn SecureFreeS(ptr: ?*const c_void, depth: c_int) void { }
+    pub inline fn Alloc(ptr: ?*const anyopaque, size: usize) void {
+        _ = ptr; 
+        _ = size;
+    }
+    pub inline fn Free(ptr: ?*const anyopaque) void { 
+        _ = ptr;
+    }
+    pub inline fn SecureAlloc(ptr: ?*const anyopaque, size: usize) void { 
+        _ = ptr;
+        _ = size;
+    }
+    pub inline fn SecureFree(ptr: ?*const anyopaque) void {
+        _ = ptr;
+    }
+    pub inline fn AllocS(ptr: ?*const anyopaque, size: usize, depth: c_int) void {
+        _ = ptr;
+        _ = size;
+        _ = depth;
+    }
+    pub inline fn FreeS(ptr: ?*const anyopaque, depth: c_int) void {
+        _ = ptr;
+        _ = depth;
+    }
+    pub inline fn SecureAllocS(ptr: ?*const anyopaque, size: usize, depth: c_int) void {
+        _ = ptr;
+        _ = size;
+        _ = depth;
+    }
+    pub inline fn SecureFreeS(ptr: ?*const anyopaque, depth: c_int) void {
+        _ = ptr;
+        _ = depth;
+    }
 
-    pub inline fn AllocN(ptr: ?*const c_void, size: usize, name: [*:0]const u8) void { }
-    pub inline fn FreeN(ptr: ?*const c_void, name: [*:0]const u8) void { }
-    pub inline fn SecureAllocN(ptr: ?*const c_void, size: usize, name: [*:0]const u8) void { }
-    pub inline fn SecureFreeN(ptr: ?*const c_void, name: [*:0]const u8) void { }
-    pub inline fn AllocNS(ptr: ?*const c_void, size: usize, depth: c_int, name: [*:0]const u8) void { }
-    pub inline fn FreeNS(ptr: ?*const c_void, depth: c_int, name: [*:0]const u8) void { }
-    pub inline fn SecureAllocNS(ptr: ?*const c_void, size: usize, depth: c_int, name: [*:0]const u8) void { }
-    pub inline fn SecureFreeNS(ptr: ?*const c_void, depth: c_int, name: [*:0]const u8) void { }
+    pub inline fn AllocN(ptr: ?*const anyopaque, size: usize, name: [*:0]const u8) void {
+        _ = ptr;
+        _ = size;
+        _ = name;
+    }
+    pub inline fn FreeN(ptr: ?*const anyopaque, name: [*:0]const u8) void {
+        _ = ptr;
+        _ = name;
+    }
+    pub inline fn SecureAllocN(ptr: ?*const anyopaque, size: usize, name: [*:0]const u8) void {
+        _ = ptr;
+        _ = size; 
+        _ = name;
+    }
+    pub inline fn SecureFreeN(ptr: ?*const anyopaque, name: [*:0]const u8) void {
+        _ = ptr;
+        _ = name;
+    }
+    pub inline fn AllocNS(ptr: ?*const anyopaque, size: usize, depth: c_int, name: [*:0]const u8) void {
+        _ = ptr;
+        _ = size; 
+        _ = depth;
+        _ = name;
+    }
+    pub inline fn FreeNS(ptr: ?*const anyopaque, depth: c_int, name: [*:0]const u8) void {
+        _ = ptr;
+        _ = depth;
+        _ = name;
+    }
+    pub inline fn SecureAllocNS(ptr: ?*const anyopaque, size: usize, depth: c_int, name: [*:0]const u8) void {
+        _ = ptr;
+        _ = size;
+        _ = depth;
+        _ = name;
+    }
+    pub inline fn SecureFreeNS(ptr: ?*const anyopaque, depth: c_int, name: [*:0]const u8) void {
+        _ = ptr;
+        _ = depth;
+        _ = name;
+    }
 
-    pub inline fn Message(text: []const u8) void { }
-    pub inline fn MessageL(text: [*:0]const u8) void { }
-    pub inline fn MessageC(text: []const u8, color: u32) void { }
-    pub inline fn MessageLC(text: [*:0]const u8, color: u32) void { }
-    pub inline fn MessageS(text: []const u8, depth: c_int) void { }
-    pub inline fn MessageLS(text: [*:0]const u8, depth: c_int) void { }
-    pub inline fn MessageCS(text: []const u8, color: u32, depth: c_int) void { }
-    pub inline fn MessageLCS(text: [*:0]const u8, color: u32, depth: c_int) void { }
+    pub inline fn Message(text: []const u8) void {
+        _ = text;
+    }
+    pub inline fn MessageL(text: [*:0]const u8) void {
+        _ = text;
+    }
+    pub inline fn MessageC(text: []const u8, color: u32) void {
+        _ = text;
+        _ = color;
+    }
+    pub inline fn MessageLC(text: [*:0]const u8, color: u32) void {
+        _ = text;
+        _ = color;
+    }
+    pub inline fn MessageS(text: []const u8, depth: c_int) void {
+        _ = text;
+        _ = depth;
+    }
+    pub inline fn MessageLS(text: [*:0]const u8, depth: c_int) void {
+        _ = text;
+        _ = depth;
+    }
+    pub inline fn MessageCS(text: []const u8, color: u32, depth: c_int) void {
+        _ = text;
+        _ = color;
+        _ = depth;
+    }
+    pub inline fn MessageLCS(text: [*:0]const u8, color: u32, depth: c_int) void {
+        _ = text;
+        _ = color;
+        _ = depth;
+    }
 
     pub inline fn FrameMark() void { }
-    pub inline fn FrameMarkNamed(name: [*:0]const u8) void { }
-    pub inline fn FrameMarkStart(name: [*:0]const u8) void { }
-    pub inline fn FrameMarkEnd(name: [*:0]const u8) void { }
-    pub inline fn FrameImage(image: ?*const c_void, width: u16, height: u16, offset: u8, flip: c_int) void { }
+    pub inline fn FrameMarkNamed(name: [*:0]const u8) void {
+        _ = name;
+    }
+    pub inline fn FrameMarkStart(name: [*:0]const u8) void {
+        _ = name;
+    }
+    pub inline fn FrameMarkEnd(name: [*:0]const u8) void {
+        _ = name;
+    }
+    pub inline fn FrameImage(image: ?*const anyopaque, width: u16, height: u16, offset: u8, flip: c_int) void { 
+        _ = image;
+        _ = width;
+        _ = height;
+        _ = offset;
+        _ = flip;
+    }
 
-    pub inline fn PlotF(name: [*:0]const u8, val: f64) void { }
-    pub inline fn PlotU(name: [*:0]const u8, val: u64) void { }
-    pub inline fn PlotI(name: [*:0]const u8, val: i64) void { }
-    pub inline fn AppInfo(text: []const u8) void { }
+    pub inline fn PlotF(name: [*:0]const u8, val: f64) void {
+        _ = name;
+        _ = val;
+    }
+    pub inline fn PlotU(name: [*:0]const u8, val: u64) void {
+        _ = name;
+        _ = val;
+    }
+    pub inline fn PlotI(name: [*:0]const u8, val: i64) void {
+        _ = name;
+        _ = val;
+    }
+    pub inline fn AppInfo(text: []const u8) void {
+        _ = text;
+    }
 };
 
 const tracy_full = struct {
@@ -202,56 +354,56 @@ const tracy_full = struct {
         return initZone(src, name, color, depth);
     }
 
-    pub inline fn Alloc(ptr: ?*const c_void, size: usize) void {
+    pub inline fn Alloc(ptr: ?*const anyopaque, size: usize) void {
         if (has_callstack_support) {
             c.___tracy_emit_memory_alloc_callstack(ptr, size, callstack_enabled, 0);
         } else {
             c.___tracy_emit_memory_alloc(ptr, size, 0);
         }
     }
-    pub inline fn Free(ptr: ?*const c_void) void {
+    pub inline fn Free(ptr: ?*const anyopaque) void {
         if (has_callstack_support) {
             c.___tracy_emit_memory_free_callstack(ptr, callstack_enabled, 0);
         } else {
             c.___tracy_emit_memory_free(ptr, size, 0);
         }
     }
-    pub inline fn SecureAlloc(ptr: ?*const c_void, size: usize) void {
+    pub inline fn SecureAlloc(ptr: ?*const anyopaque, size: usize) void {
         if (has_callstack_support) {
             c.___tracy_emit_memory_alloc_callstack(ptr, size, callstack_enabled, 1);
         } else {
             c.___tracy_emit_memory_alloc(ptr, size, 1);
         }
     }
-    pub inline fn SecureFree(ptr: ?*const c_void) void {
+    pub inline fn SecureFree(ptr: ?*const anyopaque) void {
         if (has_callstack_support) {
             c.___tracy_emit_memory_free_callstack(ptr, callstack_enabled, 1);
         } else {
             c.___tracy_emit_memory_free(ptr, size, 1);
         }
     }
-    pub inline fn AllocS(ptr: ?*const c_void, size: usize, depth: c_int) void {
+    pub inline fn AllocS(ptr: ?*const anyopaque, size: usize, depth: c_int) void {
         if (has_callstack_support) {
             c.___tracy_emit_memory_alloc_callstack(ptr, size, depth, 0);
         } else {
             c.___tracy_emit_memory_alloc(ptr, size, 0);
         }
     }
-    pub inline fn FreeS(ptr: ?*const c_void, depth: c_int) void {
+    pub inline fn FreeS(ptr: ?*const anyopaque, depth: c_int) void {
         if (has_callstack_support) {
             c.___tracy_emit_memory_free_callstack(ptr, depth, 0);
         } else {
             c.___tracy_emit_memory_free(ptr, 0);
         }
     }
-    pub inline fn SecureAllocS(ptr: ?*const c_void, size: usize, depth: c_int) void {
+    pub inline fn SecureAllocS(ptr: ?*const anyopaque, size: usize, depth: c_int) void {
         if (has_callstack_support) {
             c.___tracy_emit_memory_alloc_callstack(ptr, size, depth, 1);
         } else {
             c.___tracy_emit_memory_alloc(ptr, size, 1);
         }
     }
-    pub inline fn SecureFreeS(ptr: ?*const c_void, depth: c_int) void {
+    pub inline fn SecureFreeS(ptr: ?*const anyopaque, depth: c_int) void {
         if (has_callstack_support) {
             c.___tracy_emit_memory_free_callstack(ptr, depth, 1);
         } else {
@@ -259,56 +411,56 @@ const tracy_full = struct {
         }
     }
 
-    pub inline fn AllocN(ptr: ?*const c_void, size: usize, name: [*:0]const u8) void {
+    pub inline fn AllocN(ptr: ?*const anyopaque, size: usize, name: [*:0]const u8) void {
         if (has_callstack_support) {
             c.___tracy_emit_memory_alloc_callstack_named(ptr, size, callstack_enabled, 0, name);
         } else {
             c.___tracy_emit_memory_alloc_named(ptr, size, 0, name);
         }
     }
-    pub inline fn FreeN(ptr: ?*const c_void, name: [*:0]const u8) void {
+    pub inline fn FreeN(ptr: ?*const anyopaque, name: [*:0]const u8) void {
         if (has_callstack_support) {
             c.___tracy_emit_memory_free_callstack_named(ptr, callstack_enabled, 0, name);
         } else {
             c.___tracy_emit_memory_free_named(ptr, 0, name);
         }
     }
-    pub inline fn SecureAllocN(ptr: ?*const c_void, size: usize, name: [*:0]const u8) void {
+    pub inline fn SecureAllocN(ptr: ?*const anyopaque, size: usize, name: [*:0]const u8) void {
         if (has_callstack_support) {
             c.___tracy_emit_memory_alloc_callstack_named(ptr, size, callstack_enabled, 1, name);
         } else {
             c.___tracy_emit_memory_alloc_named(ptr, size, 1, name);
         }
     }
-    pub inline fn SecureFreeN(ptr: ?*const c_void, name: [*:0]const u8) void {
+    pub inline fn SecureFreeN(ptr: ?*const anyopaque, name: [*:0]const u8) void {
         if (has_callstack_support) {
             c.___tracy_emit_memory_free_callstack_named(ptr, callstack_enabled, 1, name);
         } else {
             c.___tracy_emit_memory_free_named(ptr, 1, name);
         }
     }
-    pub inline fn AllocNS(ptr: ?*const c_void, size: usize, depth: c_int, name: [*:0]const u8) void {
+    pub inline fn AllocNS(ptr: ?*const anyopaque, size: usize, depth: c_int, name: [*:0]const u8) void {
         if (has_callstack_support) {
             c.___tracy_emit_memory_alloc_callstack_named(ptr, size, depth, 0, name);
         } else {
             c.___tracy_emit_memory_alloc_named(ptr, size, 0, name);
         }
     }
-    pub inline fn FreeNS(ptr: ?*const c_void, depth: c_int, name: [*:0]const u8) void {
+    pub inline fn FreeNS(ptr: ?*const anyopaque, depth: c_int, name: [*:0]const u8) void {
         if (has_callstack_support) {
             c.___tracy_emit_memory_free_callstack_named(ptr, depth, 0, name);
         } else {
             c.___tracy_emit_memory_free_named(ptr, 0, name);
         }
     }
-    pub inline fn SecureAllocNS(ptr: ?*const c_void, size: usize, depth: c_int, name: [*:0]const u8) void {
+    pub inline fn SecureAllocNS(ptr: ?*const anyopaque, size: usize, depth: c_int, name: [*:0]const u8) void {
         if (has_callstack_support) {
             c.___tracy_emit_memory_alloc_callstack_named(ptr, size, depth, 1, name);
         } else {
             c.___tracy_emit_memory_alloc_named(ptr, size, 1, name);
         }
     }
-    pub inline fn SecureFreeNS(ptr: ?*const c_void, depth: c_int, name: [*:0]const u8) void {
+    pub inline fn SecureFreeNS(ptr: ?*const anyopaque, depth: c_int, name: [*:0]const u8) void {
         if (has_callstack_support) {
             c.___tracy_emit_memory_free_callstack_named(ptr, depth, 1, name);
         } else {
@@ -357,7 +509,7 @@ const tracy_full = struct {
     pub inline fn FrameMarkEnd(name: [*:0]const u8) void {
         c.___tracy_emit_frame_mark_end(name);
     }
-    pub inline fn FrameImage(image: ?*const c_void, width: u16, height: u16, offset: u8, flip: c_int) void {
+    pub inline fn FrameImage(image: ?*const anyopaque, width: u16, height: u16, offset: u8, flip: c_int) void {
         c.___tracy_emit_frame_image(image, width, height, offset, flip);
     }
 
