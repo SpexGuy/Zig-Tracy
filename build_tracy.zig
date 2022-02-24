30db753bb2c053d1abe8eec1dbb1cdfabe489143
Aksel Hjerpbakk <akselhallajs@gmail.com>
2022-02-24 16:26:53 +0100

    Update to zig 0.9.0
    * remove deprecated call to step.addBuildOption
    
    * mark unused variables in stub
    
    * use new is_test
    
    * add missing arguments
    
    * add build options using new zig api
    
    * run zig fmto

--- build_tracy.zig
+++ build_tracy.zig
@@ -2,12 +2,15 @@ const std = @import("std");
 const Builder = std.build.Builder;
 const LibExeObjStep = std.build.LibExeObjStep;
 
-pub fn link(step: *LibExeObjStep, opt_path: ?[]const u8) void {
-    step.addBuildOption(bool, "tracy_enabled", opt_path != null);
+/// Build required sources, use tracy by importing "tracy.zig"
+pub fn link(b: *Builder, step: *LibExeObjStep, opt_path: ?[]const u8) void {
+    const step_options = b.addOptions();
+    step.addOptions("build_options", step_options);
+    step_options.addOption(bool, "tracy_enabled", opt_path != null);
+
     if (opt_path) |path| {
-        step.addIncludeDir(path);
-        const tracy_client_source_path = std.fs.path.join(step.builder.allocator, &.{path, "TracyClient.cpp"})
-            catch unreachable;
+        step.addIncludePath(path);
+        const tracy_client_source_path = std.fs.path.join(step.builder.allocator, &.{ path, "TracyClient.cpp" }) catch unreachable;
         step.addCSourceFile(tracy_client_source_path, &[_][]const u8{
             "-DTRACY_ENABLE",
             // MinGW doesn't have all the newfangled windows features,
